852a87a444f264c49ea18f67fbd0fdec198967db
theonotdore <thiagocfelippe@yahoo.co.uk>
2013-07-10 23:04:34 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Smartfinance::Application.config.secret_key_base = 'b6663ca332fd38e46714480f718130ffd9a15774b1943659161b2d79b2d90b014fc1f8e1416aacd464cc6c8d638d6a70b7a781b5a34a3a5346a3bf6dad8f298a'
